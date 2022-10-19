37cc3075f1978bed65f9e9291a4d584cdf626131
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2022-10-19 16:13:28 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+}
